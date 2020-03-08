2992ef936cdc4546bca649b5e86db6c82b898081
Gilroy Menezes <gilroymenezes@gmail.com>
2020-03-09 00:12:31 +0530

    Initial Commit

---
+++ console/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Test();
+        }
+
+        static void Test() 
+        {
+            for(int i = 0; i < 100; ++i)
+            {
+                if (i % 3 == 0 && i % 5 == 0) 
+                    Console.WriteLine("FizzBuzz");
+                else if (i % 3 == 0)
+                    Console.WriteLine("Fizz");
+                else if (i % 5 == 0)
+                    Console.WriteLine("Buzz");
+                else 
+                    Console.WriteLine(i);
+            }
+        }
+    }
+}
