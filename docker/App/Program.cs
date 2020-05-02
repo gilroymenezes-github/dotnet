bffd34a1ea7a21c552907b40847dbaf58d5ca2ce
Gilroy Menezes <gilroymenezes@gmail.com>
2020-05-03 00:48:26 +0530

    Initial commit

---
+++ docker/App/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace NetCore.Docker
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            //Console.WriteLine("Hello World!");
+            var counter = 0;
+            int v = args.Length != 0
+                        ? Convert.ToInt32(args[0])
+                        : -1;
+            while (v == -1 || counter < v)
+            {
+                Console.WriteLine($"Counter: {++counter}");
+                await Task.Delay(1000);
+            }
+        }
+    }
+}
