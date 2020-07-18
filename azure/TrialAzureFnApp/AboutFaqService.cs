924fd436b2df3aa440d6877386858758d788bdd6
gilroymenezes-github <gilroymenezes@hotmail.com>
2020-07-18 20:01:44 +0530

    azure function etc

---
+++ azure/TrialAzureFnApp/AboutFaqService.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using TrialAzureFnApp.Models;
+
+namespace TrialAzureFnApp
+{
+    class AboutFaqService
+    {
+        [FunctionName("AboutFaq")]
+        public static async Task<IActionResult> Run(
+            [HttpTrigger(Microsoft.Azure.WebJobs.Extensions.Http.AuthorizationLevel.Anonymous, "get", Route =null)] HttpRequest req,
+            [CosmosDB(
+                databaseName: "TrialDb", collectionName:"TrialContainer", ConnectionStringSetting ="CosmosDbConnectionString"
+            )] IEnumerable<AboutFaq> itemSet,
+            ILogger log)
+        {
+            log.LogInformation("Data fetched from TrialDb");
+            return new OkObjectResult(itemSet);
+        }
+    }
+}
---
+++ azure/TrialAzureFnApp/Models/AboutFaq.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TrialAzureFnApp.Models
+{
+    class AboutFaq
+    {
+        public string Id { get; set; }
+        public string Question { get; set; }
+        public string Answer { get; set; }
+    }
+}
