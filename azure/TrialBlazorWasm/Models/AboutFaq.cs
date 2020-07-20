f6763c6bf1fb9b6d8eea48df9455aefdab92b76a
gilroymenezes-github <gilroymenezes@hotmail.com>
2020-07-20 16:52:25 +0530

    TrialFunctionApp Blazor client

---
+++ azure/TrialBlazorWasm/Models/AboutFaq.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TrialBlazorWasm.Models
+{
+    public class AboutFaq
+    {
+        public string Id { get; set; }
+        public string Question { get; set; }
+        public string Answer { get; set; }
+    }
+}
