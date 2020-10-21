7c7fe9deca4f90fb86a1ba16e4254894af2fb140
gilroymenezes-github <gilroymenezes@hotmail.com>
2020-10-21 17:24:32 +0530

    tsconfig.json for browser compliant transpilation

--- blazor/BlazorRcl/Services/JsInteropService.cs
+++ blazor/BlazorRcl/Services/JsInteropService.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace BlazorRcl.Components
+namespace BlazorRcl.Services
 {
     public class JsInteropService
     {
--- blazor/BlazorServerSide/Startup.cs
+++ blazor/BlazorServerSide/Startup.cs
@@ -10,6 +10,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Blazor.Services;
+using BlazorRcl.Services;
 
 namespace Blazor
 {
