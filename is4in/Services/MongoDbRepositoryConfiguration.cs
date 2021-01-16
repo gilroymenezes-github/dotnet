afc2d7d684274e1c98e990522c6ae004d4231fef
gilroymenezes-github <gilroymenezes@hotmail.com>
2021-01-16 13:36:57 +0530

    idserver stuff moved out

--- is4in/Services/MongoDbRepositoryConfiguration.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace is4in.Services
-{
-    public class MongoDbRepositoryConfiguration
-    {
-        public string ConnectionString { get; set; }
-        public string DatabaseName { get; set; }
-    }
-}
