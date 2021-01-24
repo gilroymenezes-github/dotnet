2bb01aafe22234101c50bffa15292cc8819265a3
gilroymenezes-github <gilroymenezes@hotmail.com>
2021-01-24 16:15:49 +0530

    testing changes

--- hosting/Hosting/Controllers/ValuesController.cs
+++ hosting/Hosting/Controllers/ValuesController.cs
@@ -16,7 +16,7 @@ namespace Hosting.Controllers
         [HttpGet]
         public IEnumerable<string> Get()
         {
-            return new string[] { "value1", "value2" };
+            return new string[] { "value1", "value2", "value3" };
         }
 
         // GET api/<ValuesController>/5
