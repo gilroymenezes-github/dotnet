4e6d1d88594141e0faaf067e4f0894cf30bebe0f
gilroymenezes-github <gilroymenezes@hotmail.com>
2021-09-07 17:55:20 +0530

    Regex with string parser

---
+++ regexer/StringBreaker/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Text.RegularExpressions;
+
+var inputString = @"P22101,TTM Technologies,"" $ 27,894 "",1/2/2020,1/2/2020,0,On-Time,Material,12/19/2019,  14 ,  -   ,1,5,"" $ 21,029 "","" $ 10,000 "","" $ 100,000 "",Comcast,215,1";
+
+Console.WriteLine($"Input String: {inputString}");
+
+var regex = new Regex("(?<=^|,)(\"(?:[^\"]|\"\")*\"|[^,]*)");
+
+var splitString = regex.Matches(inputString).ToList();
+
+foreach (var str in splitString) Console.WriteLine(str);
+
+Console.ReadKey();
