917ce9758e8cd490d4d74fd74b1ea058b5a80aac
gilroymenezes-github <gilroymenezes@gmail.com>
2022-01-27 08:50:15 +0530

    Cleanup on defaults in VisualFinder

--- azure/BlazorServerApps/VisualFinder/Program.cs
+++ azure/BlazorServerApps/VisualFinder/Program.cs
@@ -1,13 +1,11 @@
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
-using VisualFinder.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
-builder.Services.AddSingleton<WeatherForecastService>();
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("http://localhost:7071/api/") });
 
 var app = builder.Build();
